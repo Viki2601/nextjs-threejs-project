82c51cd0ec52a05ca8e604edd0ba07843732f267
Vishnu Muthukumar <mcvicky2601@gmail.com>
2025-04-01 07:07:38 +0530

    Cursor&Icon

---
+++ src/app/cursor/page.jsx
@@ -0,0 +1,22 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import { motion } from "framer-motion";
+
+function Cursor() {
+    const [position, setPosition] = useState({ x: 0, y: 0 });
+    useEffect(() => {
+        const mouseMove = (e) => {
+            setPosition({ x: e.clientX, y: e.clientY })
+        };
+        window.addEventListener("mousemove", mouseMove);
+        return () => {
+            window.removeEventListener("mousemove", mouseMove);
+        };
+    }, []);
+    return (
+        <motion.div className='w-[25px] h-[25px] bg-white mix-blend-difference rounded-full border fixed z-[9999]' animate={{ x: position.x + 10, y: position.y + 10 }}></motion.div>
+    )
+}
+
+export default Cursor
--- src/app/page.js
+++ src/app/page.js
@@ -3,10 +3,12 @@ import About from "./components/about";
 import DetailedView from "./components/detailedView";
 import SAB from "./components/sab";
 import Testimonials from "./components/testimonials";
+import Cursor from "./cursor/page";
 
 export default function Home() {
   return (
     <main>
+      <Cursor />
       <section className="py-5 w-full flex justify-center items-center h-screen ">
         <Landing />
       </section>
