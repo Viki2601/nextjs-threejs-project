850e5c639006bb18e0364a577f9f1cf0f67ece37
Viki2601 <mcvicky2601@gmail.com>
2025-09-23 08:15:19 +0530

    API Fixed

--- src/app/hrPortal/page.jsx
+++ src/app/hrPortal/page.jsx
@@ -115,7 +115,7 @@ export default function Page() {
   // Fetching Jobs
   const fetchJobs = async () => {
     try {
-      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL_PROD}/jobs`);
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL_PROD}/getJobs`);
       if (!response.ok) throw new Error("Failed to fetch jobs");
       const jobs = await response.json();
       console.log("Fetched jobs:", jobs);
